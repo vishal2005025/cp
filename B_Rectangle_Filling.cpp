07380a158e3fa74c9ce39c85c05f022a4c4a7597
vishal2005025 <vishalmalyan2005@gmail.com>
2024-04-28 15:14:49 +0530

    complete

--- B_Rectangle_Filling.cpp
+++ B_Rectangle_Filling.cpp
@@ -26,36 +26,32 @@ signed main()
                         break;
                     }
                 }
-                cout<<b<<endl;
             }
-            else if(s[0][0]==s[n-1][0]){
+             if(s[0][0]==s[n-1][0]){
                 for (int i=1;i<n-1;i++){
                     if(s[i][m-1]==s[0][0]) {
                        b="YES";
                         break;
                     }
                 }
-                cout<<b<<endl;
             }
-           else  if(s[n-1][m-1]==s[n-1][0]){
+            if(s[n-1][m-1]==s[n-1][0]){
                 for (int i=1;i<m-1;i++){
                     if(s[0][i]==s[n-1][m-1]) {
                        b="YES";
                         break;
                     }
                 }
-                cout<<b<<endl;
             }
-            else if(s[n-1][m-1]==s[0][m-1]){
+            if(s[n-1][m-1]==s[0][m-1]){
                 for (int i=1;i<n-1;i++){
                     if(s[i][0]==s[n-1][m-1]) {
                        b="YES";
                         break;
                     }
                 }
-                cout<<b<<endl;
             }
-            
+             cout<<b<<endl;
 
             
         }
